f1f8f3f5bb9479dccda63207f3d8f16e0abe9891
zizzah <chukwudiokolo416@gmail.com>
2026-04-24 12:23:19 +0100

    fix migration 058f2bcd18ed: use DO block to skip existing constraint

--- alembic/versions/058f2bcd18ed_add_non_negative_stock_constraint.py
+++ alembic/versions/058f2bcd18ed_add_non_negative_stock_constraint.py
@@ -6,7 +6,6 @@ Create Date: 2026-04-22 17:15:03.630339
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.exc import ProgrammingError
 
 revision: str = '058f2bcd18ed'
 down_revision: Union[str, Sequence[str], None] = '91735bd7ea66'
@@ -14,14 +13,20 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
-    try:
-        op.create_check_constraint(
-            'quantity_in_stock_non_negative',
-            'products',
-            'quantity_in_stock >= 0'
-        )
-    except ProgrammingError:
-        pass  # constraint already exists in DB
+    op.execute("""
+        DO `push
+        BEGIN
+            IF NOT EXISTS (
+                SELECT 1 FROM pg_constraint
+                WHERE conname = 'quantity_in_stock_non_negative'
+            ) THEN
+                ALTER TABLE products
+                ADD CONSTRAINT quantity_in_stock_non_negative
+                CHECK (quantity_in_stock >= 0);
+            END IF;
+        END
+        `push;
+    """)
 
 def downgrade() -> None:
     op.drop_constraint(
